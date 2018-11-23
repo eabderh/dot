347c34160c5ef6e3f59ffcfd9fd5cadb04dadac0
elias <elias@bytekite.io>
2018-11-23 12:13:38 +0000

    changed where old dot files are put

--- room.sh
+++ room.sh
@@ -5,10 +5,10 @@
 dir="$PWD"
 dest="$1"
 
-mkdir -p "$dest/.tmp/dotfiles"
+mkdir -p "$dest/tmp/dotfiles"
 for i in $(find dist -maxdepth 1 -name '.*' ); do
 	cd $dest
-	mv $(basename $i) .tmp/dotfiles/.
+	mv $(basename $i) tmp/dotfiles/.
 	cd $dir
 done
 
