f131041a8ac2a6fb949f3d91f133b5d63756e81b
elias <elias@bytekite.io>
2021-02-24 15:18:10 +0100

    git: changed bash aliases to functions

--- src/git/.config/bash/config.d/git.bash
+++ src/git/.config/bash/config.d/git.bash
@@ -1,33 +1,55 @@
 
-alias g='git'
+
+# g -> git
+function g() 	{ git $@ 					;}; export -f g
+
 
 # status
-alias gs='git status'
 alias gs.='git status .'
-alias gdif='git diff'
-alias glog='git log --oneline'
-alias ghis='git log --oneline --graph --all --decorate --abbrev-commit'
+function gs() 	{ git status --short $@ 	;}; export -f gs
+function gdif() { git diff $@ 				;}; export -f gdif
+function glog() { git log --oneline $@ 		;}; export -f glog
+
 
 # commits
-alias gadd='git add'
-alias gaa='git add --all'
-alias gcom='git commit'
+function gadd() { git add $@ 				;}; export -f gadd
+function gaa() 	{ git add --all $@ 			;}; export -f gaa
+function gcom() { git commit $@ 			;}; export -f gcom
+
 
 # branches
-alias gche='git checkout'
-alias gbra='git branch'
+function gche() { git checkout $@ 			;}; export -f gche
+function gbra() { git branch $@ 			;}; export -f gbra
+
 
 # remotes
-alias gpu='git push'
-alias gfe='git fetch'
-alias gpl='git pull'
+function gfe() 	{ git fetch $@ 				;}; export -f gfe
+function gpu() 	{ git push $@ 				;}; export -f gpu
+function gpl() 	{ git pull $@ 				;}; export -f gpl
 
-# submodules
-alias gsub='git submodule'
-alias gsubup='git submodule update --recursive --remote'
+
+# update-server-info
+function gup() 	{ git update-server-info $@ ;}; export -f gup
+
+
+# history
+function ghis() {
+	git log \
+		--oneline \
+		--graph \
+		--all \
+		--decorate \
+		--abbrev-commit $@
+}; export -f ghis
+
 
 # git gross - add, commit, push (named so because it isn't the best shortcut)
-alias gross='git add . ; git commit ; git push'
+function gros() {
+	git add .
+	git commit
+	git push
+}; export -f gros
+
 
 # TODO: put this into shell script
 function gist() {
@@ -38,9 +60,24 @@ function gist() {
 	else
 		git diff --stat HEAD~$1 HEAD~$2
 	fi
-}
-export -f gist
-# update-server-info
-alias gup='git update-server-info'
+}; export -f gist
+
+
+# check if dirty
+function gss() {
+	if [ -z "$(git status --porcelain)" ]; then
+		return 0
+	fi
+	echo dirty
+	return 1
+}; export -f gss
+
+
+# submodules
+function gsub() { git submodule $@ 	;}; export -f gsub
+function gsubup() {
+	git submodule update --recursive --remote $@
+}; export -f gsubup
+
 
 
