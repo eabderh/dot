6c2c43564218e7811effe750541df0ef2e93b8b3
elias <elias@bytekite.io>
2021-03-01 19:05:16 +0100

    git: reworked git log and .gitconfig

--- src/git/.config/bash/config.d/git.bash
+++ src/git/.config/bash/config.d/git.bash
@@ -1,5 +1,9 @@
 
 
+# basic functions
+
+#------------------------------------------------------------------------------
+
 # g -> git
 function g() 	{ git $@ 					;}; export -f g
 
@@ -8,7 +12,6 @@ function g() 	{ git $@ 					;}; export -f g
 alias gs.='git status .'
 function gs() 	{ git status --short $@ 	;}; export -f gs
 function gdif() { git diff $@ 				;}; export -f gdif
-function glog() { git log --oneline $@ 		;}; export -f glog
 
 
 # commits
@@ -32,16 +35,64 @@ function gpl() 	{ git pull $@ 				;}; export -f gpl
 function gup() 	{ git update-server-info $@ ;}; export -f gup
 
 
-# history
+
+
+# git log functions
+#------------------------------------------------------------------------------
+
+# format fragments
+
+HASH='%C(white)%h' # 7
+TIME='  %C(blue)%<(13)%ad' # 15
+AUTHOR_EMAIL='  %C(blue)%<(20,trunc)%ae' # 22
+REF='%C(yellow)%<(35)%d'
+INDENT='%C(white)-------'
+#SUBJECT='  %C(cyan)%<(50,trunc)%s'
+
+# basic format without subject
+export GITLOGFORMAT="$HASH$TIME$AUTHOR_EMAIL$REF%n$INDENT"
+unset HASH TIME AUTHOR_EMAIL REF INDENT # SUBJECT
+
+# short git log version
+function glog() {
+	SUBJECT='  %C(reset)%<(50,trunc)%s'
+	git log \
+		--pretty=format:"$GITLOGFORMAT$SUBJECT" \
+		--date=relative \
+		"$@"
+		#--graph \
+}; export -f glog
+
+# long git log version (added stat and summary)
+function glogs() {
+	SUBJECT='  %C(cyan)%<(50,trunc)%s'
+	git \
+		-c color.diff.new=white \ # disable colored +/- from --stat
+		-c color.diff.old=white \ # disable colored +/- from --stat
+		log \
+		--pretty=format:"$GITLOGFORMAT$SUBJECT" \
+		--date=relative \
+		--stat \
+		--summary \
+		"$@"
+}; export -f glogs
+
+# historic git log version
 function ghis() {
 	git log \
-		--oneline \
-		--graph \
+		--format=medium \
+		--date=iso \
+		--stat \
 		--all \
 		--decorate \
 		--abbrev-commit $@
+		#--graph \
 }; export -f ghis
 
+
+
+# other functions
+#------------------------------------------------------------------------------
 
 # git gross - add, commit, push (named so because it isn't the best shortcut)
 function gros() {
@@ -53,13 +104,11 @@ function gros() {
 
 # TODO: put this into shell script
 function gist() {
-	if [ -z "$1" ]; then
-		git diff --stat HEAD~ HEAD
-	elif [ -z "$2" ]; then
-		git diff --stat HEAD~$1 HEAD
-	else
-		git diff --stat HEAD~$1 HEAD~$2
-	fi
+	# how far back to start
+	START="HEAD~${1:-0}"
+	# how far back to end
+	END="HEAD~${2:-0}"
+	git --no-pager diff --stat --summary $START $END
 }; export -f gist
 
 
