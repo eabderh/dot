d308706c69accebd145ab5b4ba7c469d7444c10e
elias <elias@bytekite.io>
2021-03-03 15:51:09 +0100

    bash: added shopt glob options

--- src/bash/.config/bash/config.d/shopt.bash
+++ src/bash/.config/bash/config.d/shopt.bash
@@ -1,6 +1,11 @@
 
 # command line
+
+# allow ** in globs
 shopt -s globstar
+# match dotfiles (files starting with a '.')
+shopt -s dotglob
+# allow editing the commandline with $EDITOR
 shopt -s histverify
 # https://askubuntu.com/a/318746
 shopt -s direxpand
