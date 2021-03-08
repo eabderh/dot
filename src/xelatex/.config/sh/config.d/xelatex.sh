c7a7ad7df6b29e97ff404204577f434006e34e69
elias <elias@bytekite.io>
2021-03-08 20:53:28 +0100

    latex: changed font paths

--- src/xelatex/.config/sh/config.d/xelatex.sh
+++ src/xelatex/.config/sh/config.d/xelatex.sh
@@ -1,6 +1,6 @@
 
 export TEXHOME="$LOCALROOT/usr/lib/latex"
 export TEXINPUTS="$TEXHOME//:"
-export TTFONTS="/usr/share/fonts//:$LOCALROOT/usr/share/fonts//:"
-export OPENTYPEFONTS="$LOCALROOT/usr/share/fonts//:"
+export TTFONTS="/usr/share/fonts//:"
+export OPENTYPEFONTS="/usr/share/fonts//:"
 
