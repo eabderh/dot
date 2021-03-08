1e635d3dd2ed7535e6c9df33054e195e44f9ad77
elias <elias@bytekite.io>
2021-03-08 21:01:07 +0100

    squash

--- src/xelatex/.config/sh/config.d/xelatex.sh
+++ src/xelatex/.config/sh/config.d/xelatex.sh
@@ -1,5 +1,5 @@
 
-export TEXHOME="$LOCALROOT/usr/lib/latex"
+export TEXHOME="$HOME/dev/domains/latex"
 export TEXINPUTS="$TEXHOME//:"
 export TTFONTS="/usr/share/fonts//:"
 export OPENTYPEFONTS="/usr/share/fonts//:"
