824a71ae51871cda392c05ff0dab166e4df3a133
elias <elias@bytekite.io>
2021-03-09 10:20:57 +0100

    lego: updated root

--- src/lego/.config/sh/config.d/lego.sh
+++ src/lego/.config/sh/config.d/lego.sh
@@ -1,5 +1,5 @@
 
-export LEGO_PATH="$LOCALROOT/etc/lego-ssl"
+export LEGO_PATH="$HOME/dev/domains/lego-ssl/tree/etc/lego-ssl"
 # lego namecheap
 export NAMECHEAP_PASS_ROOT="personal/legals/accounts/namecheap"
 export NAMECHEAP_API_USER="bananalias"
