f7ee352af07951cdef661243f2d9bc7d3fd8003a
elias <elias@bytekite.io>
2021-03-09 10:21:15 +0100

    golang: updated GOROOT

--- src/golang/.config/sh/config.d/golang.sh
+++ src/golang/.config/sh/config.d/golang.sh
@@ -2,7 +2,7 @@
 export GOVERSION="1.15.5"
 export GOOS="linux"
 export GOARCH="$(dpkg --print-architecture)"
-export GOROOT="$HOME/.local/usr/local/go"
+export GOROOT="$HOME/dev/domains/golang"
 export GOPATH="$HOME/dev/src/go"
 export GO111MODULE=on
 export GOPRIVATE="git.bananalias.xyz/*"
