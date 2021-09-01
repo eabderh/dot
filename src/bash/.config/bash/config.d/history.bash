cdc80ba2e25e9c85e1290a9baf81c413bc2b3d31
elias <elias@bytekite.io>
2021-09-01 13:07:55 +0200

    bash: history: fixed $PROMPT_COMMAND addition

--- src/bash/.config/bash/config.d/history.bash
+++ src/bash/.config/bash/config.d/history.bash
@@ -4,5 +4,10 @@ export HISTROOT="$HOME/dev/domains/bash/history.d"
 export HISTFILE="$HISTROOT/$(tty | sed -e 's;/;;g')"
 export HISTCONTROL=ignorespace:ignoredups:erasedups
 export HISTSIZE="1000"
-export PROMPT_COMMAND="history -a"
+
+
+if [[ "$PROMPT_COMMAND_HISTORY" = "" ]]; then
+	export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+	export PROMPT_COMMAND_HISTORY="true"
+fi
 
