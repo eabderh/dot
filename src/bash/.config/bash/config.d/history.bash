c0e12c4336ab4c769645f93ff23d23889445903a
elias <elias@bytekite.io>
2021-09-06 20:23:22 +0200

    bash: changed $PROMPT_COMMAND integration

--- src/bash/.config/bash/config.d/history.bash
+++ src/bash/.config/bash/config.d/history.bash
@@ -6,8 +6,8 @@ export HISTCONTROL=ignorespace:ignoredups:erasedups
 export HISTSIZE="1000"
 
 
-if [[ "$PROMPT_COMMAND_HISTORY" = "" ]]; then
-	export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
-	export PROMPT_COMMAND_HISTORY="true"
-fi
+#if [[ "$PROMPT_COMMAND_HISTORY" = "" ]]; then
+export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+#export PROMPT_COMMAND_HISTORY="true"
+#fi
 
