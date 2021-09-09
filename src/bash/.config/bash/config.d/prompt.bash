099bce1910ac41ba31758a149cb27bf6b11ee0f4
elias <elias@bytekite.io>
2021-09-09 11:06:22 +0200

    bash: refactored and updated prompt.bash

--- src/bash/.config/bash/config.d/prompt.bash
+++ src/bash/.config/bash/config.d/prompt.bash
@@ -28,58 +28,119 @@ PROMPT_HOST='[\h]'
 #PROMPT_PREPEND=$'\u2666' # diamond
 #PROMPT_PREPEND=$'\u258C' # square
 #PROMPT_PREPEND=$'\u25FE' # block
+PROMPT_PREPEND='◼'
+PROMPT_APPEND='⸬ '
+PROMPT_APPEND='⨉ '
+PROMPT_APPEND='⑊'
+PROMPT_APPEND='☰ '
+PROMPT_APPEND='⏥ '
+PROMPT_APPEND='◌'
+PROMPT_APPEND='᚜'
+PROMPT_APPEND='◼'
 #PROMPT_PREPEND="\[$(tput setaf 15)\]"$'\u25FE' # ◼
 #PROMPT_PREPEND="\[$(tput setaf 15)\]"$'\u25A0' # ■
-PROMPT_PREPEND="\[$(tput setaf 15)\]"$' \u25FC' # ◾
-#PROMPT_PREPEND="#"
-PROMPT_INFO_SEP='>'
-PROMPT_CMD_PREPEND='$'
-#PROMPT_CMD_PREPEND=$'\u25FE' # block
 
 
+function exitcodeswitch {
+	local PROMPT_EXITCODE="$?"
 
-function prompt_short() {
-	PROMPT=''
-	#PROMPT+="$PROMPT_STYLE_PREPEND"
-	PROMPT+="$PROMPT_PREPEND "
-	PROMPT+="$(tput setaf 15)"
-	export PS1="$PROMPT"
-}
-
-function prompt_oneline() {
-	PROMPT=''
-	#PROMPT+="$PROMPT_STYLE_PREPEND"
-	PROMPT+="$PROMPT_PREPEND "
-	#PROMPT+="$(tput setaf 244)"
-	if [ -z "$TMUX" ]; then
-		PROMPT+="$PROMPT_HOST "
+	if [ $EXITCODE != 0 ]; then
+		PS1="$PROMPT"
+	else
+		PS1="$PROMPT"
+		#PS1="$PROMPT_FAIL"
 	fi
-	PROMPT+="$PROMPT_CWD "
-	PROMPT+="$PROMPT_CMD_PREPEND "
-	PROMPT+="\[$(tput sgr0)\]"
+}
+
+if ! command -v spwd &>/dev/null; then
+	function spwd { pwd; }
+fi
+
+
+
+function dyn_prompt {
+	local EXITCODE="$?"
+	local PROMPT=''
+	#if [[ "$(pwd)" = "$HOME" ]] then
+	#fi
+
+
+	if [[ $EXITCODE != 0 ]]; then
+		PROMPT+="\[$(tput setaf 3)\]"
+		PROMPT+=" $PROMPT_PREPEND"
+	else
+		PROMPT+="\[$(tput setaf 7)\]"
+		PROMPT+=" $PROMPT_PREPEND"
+	fi
+
+
+	PROMPT+="\[$(tput setaf 8)\]"
+	#PROMPT+=' \u@\h'
+	#PROMPT+=":"
+	PROMPT+=" $(spwd 2> /dev/null)"
+
+	PROMPT+="\[$(tput setaf 8)\]"
+	PROMPT+=" $PROMPT_APPEND"
+	PROMPT+="\[$(tput setaf 7)\]"
+	PROMPT+=" "
+	#PROMPT+=" \[$(tput sgr0)\]"
+
 	export PS1="$PROMPT"
 }
 
-#function prompt_twoline() {
-#	PROMPT=''
-#	#PROMPT+='\n'
-#	#PROMPT+="$PROMPT_PREPEND "
-#	#PROMPT+="$PROMPT_STYLE_INFO"
-#	#PROMPT+="$PROMPT_USER $PROMPT_INFO_SEP "
-#	PROMPT+="$PROMPT_STYLE_PREPEND"
-#	PROMPT+="$PROMPT_PREPEND "
-#	PROMPT+="$PROMPT_STYLE_NORMAL"
-#	PROMPT+="$PROMPT_USER "
-#	PROMPT+="$PROMPT_STYLE_SPECIAL"
-#	PROMPT+="$PROMPT_CWD \n"
-#	PROMPT+="$PROMPT_STYLE_PREPEND"
-#	PROMPT+="$PROMPT_PREPEND "
-#	PROMPT+="$PROMPT_STYLE_SPECIAL"
-#	PROMPT+="$PROMPT_CMD_PREPEND "
-#	PROMPT+="$PROMPT_STYLE_NORMAL"
-#	export PS1="$PROMPT"
-#}
-
-prompt_oneline
+export PROMPT_COMMAND="dyn_prompt;${PROMPT_COMMAND}"
 
 
+##PROMPT_PREPEND=$' \u25FC' # ◾
+###PROMPT_PREPEND="#"
+##PROMPT_INFO_SEP='>'
+##PROMPT_CMD_PREPEND='$'
+###PROMPT_CMD_PREPEND=$'\u25FE' # block
+##
+##
+##
+###function prompt_short() {
+###	PROMPT=''
+###	#PROMPT+="$PROMPT_STYLE_PREPEND"
+###	PROMPT+="$PROMPT_PREPEND "
+###	PROMPT+="$(tput setaf 15)"
+###	export PROMPT="$PROMPT"
+###}
+##
+##function prompt_oneline() {
+##	PROMPT=''
+##	PROMPT_FAIL=''
+##	PROMPT+=""\[$(tput setaf 15)\]"$PROMPT_PREPEND "
+##	PROMPT_FAIL+="$PROMPT_PREPEND "
+##	if [ -z "$TMUX" ]; then
+##		PROMPT+="$PROMPT_HOST "
+##	fi
+##	PROMPT+="$PROMPT_CWD "
+##	PROMPT+="$PROMPT_CMD_PREPEND "
+##	PROMPT+="\[$(tput sgr0)\]"
+##	export PROMPT="$PROMPT"
+##}
+##
+###function prompt_twoline() {
+###	PROMPT=''
+###	#PROMPT+='\n'
+###	#PROMPT+="$PROMPT_PREPEND "
+###	#PROMPT+="$PROMPT_STYLE_INFO"
+###	#PROMPT+="$PROMPT_USER $PROMPT_INFO_SEP "
+###	PROMPT+="$PROMPT_STYLE_PREPEND"
+###	PROMPT+="$PROMPT_PREPEND "
+###	PROMPT+="$PROMPT_STYLE_NORMAL"
+###	PROMPT+="$PROMPT_USER "
+###	PROMPT+="$PROMPT_STYLE_SPECIAL"
+###	PROMPT+="$PROMPT_CWD \n"
+###	PROMPT+="$PROMPT_STYLE_PREPEND"
+###	PROMPT+="$PROMPT_PREPEND "
+###	PROMPT+="$PROMPT_STYLE_SPECIAL"
+###	PROMPT+="$PROMPT_CMD_PREPEND "
+###	PROMPT+="$PROMPT_STYLE_NORMAL"
+###	export PS1="$PROMPT"
+###}
+##
+##prompt_oneline
+##
+##
