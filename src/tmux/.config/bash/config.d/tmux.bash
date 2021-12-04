63d55c25c82c83ffbc35522991b1402f64957749
elias <elias@bytekite.io>
2021-12-04 20:36:42 +0100

    tmux: added functions for setting prefix
    manmerge

--- src/tmux/.config/bash/config.d/tmux.bash
+++ src/tmux/.config/bash/config.d/tmux.bash
@@ -15,6 +15,11 @@ alias tms='tmux new -s'
 alias tmls='tmux list-sessions'
 
 
+function tmux_process_tree {
+	ps x --ppid $(pgrep --exact 'tmux: server')
+}; export -f tmux_process_tree
+
+
 if [[ "$TMUX" != "" ]]; then
 	function tmux_naming {
 		local STATUS=''
@@ -31,8 +36,8 @@ if [[ "$TMUX" != "" ]]; then
 		fi
 		#STATUS="────⟨ $STATUS ⟩────"
 		#STATUS="━━━━━━━━━━ $STATUS "
-		STATUS="━━━━━━━━━━━━━━━━ $STATUS "
-		#STATUS=" $STATUS "
+		#STATUS="━━━━━━━━━━━━━━━━ $STATUS "
+		STATUS=" $STATUS "
 
 		tmux select-pane -T "$STATUS"
 		tmux rename-window -t${TMUX_PANE} "$(basename $(spwd))"
@@ -41,7 +46,20 @@ if [[ "$TMUX" != "" ]]; then
 	#TMUX_CMDS+="tmux rename-window -t\${TMUX_PANE} \"\$(basename \$(spwd))\";"
 	#export PROMPT_COMMAND="${PROMPT_COMMAND}${TMUX_CMDS}"
 	export PROMPT_COMMAND="${PROMPT_COMMAND}tmux_naming;"
+
+
+	function tmuxfive {
+		tmux set-option -g prefix '5'
+		tmux bind-key '5' send-prefix
+	}; export -f tmuxfive
+
+	function tmuxctrla {
+		tmux set-option -g prefix 'C-a'
+		tmux bind-key 'C-a' send-prefix
+	}; export -f tmuxctrla
+
 fi
+
 
 
 # tmuxp
