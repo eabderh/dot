caa3eaa0ef13b633c2fd4bef9f24a089a9586975
elias <elias@bytekite.io>
2021-12-04 20:19:18 +0100

    cdup: refactored and documented
    manmerge

--- src/cdup/.config/bash/config.d/cdup.bash
+++ src/cdup/.config/bash/config.d/cdup.bash
@@ -1,10 +1,17 @@
 
+# equivalent to using 'cd ..' multiple times
+# usage: cdup <levels>
+# 	<levels>: defaults to 1
+
+
 # go up n levels
 function cdup() {
 	cd $(printf "%0.s../" $(seq 1 $1));
-	# this is a trick of parameter expansion.
+	# this is a trick of parameter expansion
 	# printf repeats for every parameter not matched directly
 }
 export -f cdup
-alias cd.='cdup'
+
+# alias of 'cdup' to 'cd.'
+function cd. { cdup $@; }; export -f cd.
 
