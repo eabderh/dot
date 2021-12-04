18acb5060e20ff2c69123b1e824ad602b1601dc2
elias <elias@bytekite.io>
2021-12-04 20:20:05 +0100

    golang: commented out unused envars
    manmerge

--- src/golang/.config/sh/config.d/golang.sh
+++ src/golang/.config/sh/config.d/golang.sh
@@ -1,16 +1,16 @@
 
-export GOVERSION="1.15.5"
+#export GOVERSION="1.15.5"
 export GOOS="linux"
 export GOARCH="$(dpkg --print-architecture)"
-export GOROOT="/usr/lib/go-1.16"
+#export GOROOT="/usr/lib/go-1.16"
 #export GOROOT="$HOME/dev/domains/golang"
 export GOPATH="$HOME/dev/src/go"
 export GO111MODULE=on
-export GOPRIVATE="git.bananalias.xyz/*"
+#export GOPRIVATE="git.bananalias.xyz/*"
 
 # private
-export GOGENNYTYPES='string,int,float64,bool,Void,Bytes,Time,Duration'
-export GOSRC="$GOPATH/src"
+#export GOGENNYTYPES='string,int,float64,bool,Void,Bytes,Time,Duration'
+#export GOSRC="$GOPATH/src"
 #alias gogen='go generate'
 
 
