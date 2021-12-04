789463d12f45521b0daa55597a88c3a63acb8ab7
elias <elias@bytekite.io>
2021-12-04 17:56:22 +0100

    git: manmerge

--- src/git/.config/bash/config.d/git.bash
+++ src/git/.config/bash/config.d/git.bash
@@ -1,17 +1,26 @@
 
 
+
+
+source /usr/share/bash-completion/completions/git
+
 # basic functions
 
 #------------------------------------------------------------------------------
 
 # g -> git
 function g() 	{ git "$@" 					;}; export -f g
+__git_complete g __git_main
 
 
 # status
 alias gs.='git status .'
-function gs() 	{ git status --short "$@" 	;}; export -f gs
+function gs() 	{ git status --short --branch "$@" 	;}; export -f gs
+__git_complete gs _git_status
+
+
 function gdif() { git diff "$@" 			;}; export -f gdif
+__git_complete gdif _git_diff
 
 
 # commits
@@ -22,17 +31,38 @@ function gcom() { git commit "$@" 			;}; export -f gcom
 
 # branches
 function gche() { git checkout "$@" 		;}; export -f gche
+__git_complete gche _git_checkout
 function gbra() { git branch "$@" 			;}; export -f gbra
+__git_complete gbra _git_branch
 
 
 # remotes
 function gfe() 	{ git fetch "$@" 			;}; export -f gfe
-function gpu() 	{ git push "$@" 			;}; export -f gpu
-function gpl() 	{ git pull "$@" 			;}; export -f gpl
+__git_complete gfe _git_fetch
+function gup() 	{ git push "$@" 			;}; export -f gup
+__git_complete gup _git_push
+function gdo() 	{ git pull "$@" 			;}; export -f gdo
+__git_complete gdo _git_pull
+function grem() { git remote -v "$@" 		;}; export -f grem
+__git_complete grem _git_remote
+
+
+# tracking
+function gref() {
+	local header='%(HEAD)'
+	local refname='%(color:blue)%(refname:short)'
+	local upstream='%(color:cyan)%(upstream:short)'
+	local tracking='%(color:yellow)%(upstream:track)'
+	git for-each-ref --color=always \
+		--format="$header $refname,$upstream $tracking" \
+		| column -t -s','
+}; export -f gref
+
+
 
 
 # update-server-info
-function gup() 	{ git update-server-info "$@" ;}; export -f gup
+function gupdate() 	{ git update-server-info "$@" ;}; export -f gupdate
 
 
 
@@ -62,9 +92,9 @@ function glog() {
 		--color \
 		--date=short \
 		--pretty=format:"$FORMAT" \
-		"$@" |
-		sed '/^\s*\x1b\[[0-9;]*m\s*$/d' |
-		less
+		#"$@" |
+		#sed '/^\s*\x1b\[[0-9;]*m\s*$/d' |
+		#less
 	#echo; echo
 		#--graph \
 }; export -f glog
@@ -113,10 +143,14 @@ function glore() {
 # other functions
 #------------------------------------------------------------------------------
 
-# git gross - add, commit, push (named so because it isn't the best shortcut)
+# git gros - add, commit, push (named so because it isn't the best shortcut)
 function gros() {
-	git add .
-	git commit
+	if [ "$1" == "" ]; then
+		echo 'commit message missing'
+		return 1
+	fi
+	git add --update
+	git commit --message="$1"
 	git push
 }; export -f gros
 
@@ -148,15 +182,15 @@ function gsubup() {
 }; export -f gsubup
 
 
-# credentials
-#------------------------------------------------------------------------------
-
-
-# submodules
-function git-credential-test() {
-	echo "git-credential-test $@" 1>&2
-	cat /dev/stdin 1>&2
-}; export -f git-credential-test
+## credentials
+##------------------------------------------------------------------------------
+#
+#
+## submodules
+#function git-credential-test() {
+#	echo "git-credential-test $@" 1>&2
+#	cat /dev/stdin 1>&2
+#}; export -f git-credential-test
 
 
 
