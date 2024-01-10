34b63267e0f36a4393a5254b35cdb8079a82aa31
elias <elias@bytekite.io>
2024-01-10 21:47:15 +0100

    pass

--- src/pass/.config/bash/config.d/pass.bash
+++ src/pass/.config/bash/config.d/pass.bash
@@ -2,7 +2,12 @@
 source $HOME/.config/bash/lib/save_function.bash
 
 
-export PASSWORD_STORE_DIR="$HOME/.pass"
+export PASSWORD_STORE_DIR="$HOME/pass"
+# in accordance with iam.swisscom.com
+export PASSWORD_STORE_CHARACTER_SET='[:alnum:]~!@#\$%^*_+\-={}][?:;,.&'
+#export PASSWORD_STORE_CHARACTER_SET='+\-='
+#export PASSWORD_STORE_CHARACTER_SET='[:alnum:]'
+
 
 function pass() {
 	# shim the 'tree' command in the /usr/bin/pass shell script
